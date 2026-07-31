024a1c2a545ded9b0c3c2fbd427a508548e9ffe8
rodrigobertol06-sys <rodrigobertol06@gmail.com>
2026-07-31 02:04:58 -0300

    Update diluição.js

--- diluição.js
+++ diluição.js
@@ -1,1 +1,13 @@
+// diluicao.js - Módulo de cálculo de diluição e custos
+export function calcularDilucao(capacidadeRecipienteMl, proporcaoProduto, custoLitroProduto) {
+    const mlProduto = capacidadeRecipienteMl / proporcaoProduto;
+    const mlAgua = capacidadeRecipienteMl - mlProduto;
+    const custoPorMl = custoLitroProduto / 1000;
+    const custoTotalUso = mlProduto * custoPorMl;
 
+    return {
+        produtoMl: mlProduto.toFixed(1),
+        aguaMl: mlAgua.toFixed(1),
+        custoUso: custoTotalUso.toFixed(2)
+    };
+}
